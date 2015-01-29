514bf271d62b95e5545b039efb0b9dabccd32852
Johannes T420 Linux <johannes@van-balen.de>
2015-01-29 19:59:42 +0100

    updated display software to work with pinout.h

--- Firmware/includes/display.c
+++ Firmware/includes/display.c
@@ -2,21 +2,39 @@
 #include "display.h"
 #include <avr/io.h>
 
+
+
+// initializing the DDRs and ports for the LED display
 void display_init() {
 	DDRB = 0xff;
 	PORTB= 0;
 	DDRC |= LED0|LED1|LED2;
 }
 
+// put a specific character up on a specific area of the seven segment display
 void display_digit(uint8_t led, uint8_t digit) {
-	PORTC &= ~(LED0|LED1|LED2); //all off
 
-	if (digit>15) {
-		//error
-		PORTB = 1;
+	PORTC &= ~(LED0|LED1|LED2); //switching all LEDs off
+
+	if (digit>16) {
+		//error in case digit was too high
+		digit = 16; // displaying only the dot
+
 	} else {
-		uint8_t sign[16] = {250, 10, 236, 174, 30, 182,  246, 42, 254, 190,  126, 214, 196, 206, 244, 116};
+
+		uint8_t sign[17] = {DISP_0, DISP_1, DISP_2, DISP_3, DISP_4, DISP_5, DISP_6, DISP_7, DISP_8, DISP_9, DISP_A, DISP_B, DISP_C, DISP_D, DISP_E, DISP_F, DISP_DOT};
 		PORTB = sign[digit];
+
 	}
-	PORTC |= led;
+
+	PORTC |= led; // enabling only the desired LED display
+}
+
+// setting up custom characters on the seven segment displays
+void display_custom(uint8_t led, uint8_t segments) {
+	PORTC &= ~(LED0|LED1|LED2); //switching all LEDs off
+
+	PORTB = segments;
+
+	PORTC |= led; // enabling only the desired LED display
 }
--- Firmware/includes/display.h
+++ Firmware/includes/display.h
@@ -9,10 +9,29 @@
 #pragma once
 
 #include <stdint.h>
+#include "pinout.h"
 
-#define LED0 (1<<PC3)
-#define LED1 (1<<PC4)
-#define LED2 (1<<PC5)
+#define DISP_OFF 0x00
+#define DISP_0 (DISP_BR|DISP_UR|DISP_UP|DISP_BOT|DISP_BL|DISP_UL)
+#define DISP_1 (DISP_BR|DISP_UR)
+#define DISP_2 (DISP_UP|DISP_UR|DISP_MID|DISP_BL|DISP_BOT)
+#define DISP_3 (DISP_UP|DISP_UR|DISP_MID|DISP_BR|DISP_BOT)
+#define DISP_4 (DISP_UL|DISP_MID|DISP_UR|DISP_BR)
+#define DISP_5 (DISP_UP|DISP_UR|DISP_MID|DISP_BR|DISP_BOT)
+#define DISP_6 (DISP_UP|DISP_UL|DISP_MID|DISP_BL|DISP_BOT|DISP_BR)
+#define DISP_7 (DISP_UP|DISP_UR|DISP_BR)
+#define DISP_8 (DISP_UP|DISP_UR|DISP_BR|DISP_UL|DISP_BL|DISP_MID|DISP_BOT)
+#define DISP_9 (DISP_UP|DISP_UR|DISP_BR|DISP_UL|DISP_MID|DISP_BOT)
+#define DISP_A (DISP_UP|DISP_UR|DISP_BR|DISP_UL|DISP_BL|DISP_MID)
+#define DISP_B (DISP_BR|DISP_UL|DISP_BL|DISP_BOT)
+#define DISP_C (DISP_UP|DISP_UL|DISP_BL|DISP_MID|DISP_BOT)
+#define DISP_D (DISP_UR|DISP_BR|DISP_BL|DISP_MID|DISP_BOT)
+#define DISP_E (DISP_UP|DISP_UL|DISP_BL|DISP_MID|DISP_BOT)
+#define DISP_F (DISP_UP|DISP_UL|DISP_BL|DISP_MID)
+#define Cdp (DISP_DOT)
+
+// ADD YOUR CUSTOM CHARS HERE!!
 
 void display_init();
 void display_digit(uint8_t led, uint8_t digit);
+void display_custom(uint8_t led, uint8_t segments);
