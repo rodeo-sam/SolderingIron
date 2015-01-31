d5d4b4e9b6f6c85b2713c1acb838ddea9ab095be
Georg von Zengen <oni303@gmail.com>
2015-01-31 17:53:45 +0100

    Added a eeprom config loader
    this enables the settings to survive software updates

---
+++ Firmware/includes/eeprom-config.c
@@ -0,0 +1,33 @@
+/*
+ * eeprom-config.c
+ *
+ * Created: Sa 31. Jan 2015 
+ * Author:  Georg von Zengen <g@vz-ge.org>
+ */ 
+
+#include <avr/eeprom.h>
+
+#include "eeprom-config.h"
+
+
+
+config_t config;
+
+config_t* config_load(void)
+{
+	uint16_t i = 0;
+	for(; i < sizeof(config); i++){
+		*(((uint8_t *) &config) + i) = eeprom_read_byte(i + CONFIG_OFFSET);
+	}
+	return &config;
+}
+
+int config_save(void)
+{
+	uint16_t i = 0;
+	for(; i < sizeof(config); i++){
+		eeprom_write_byte(i + CONFIG_OFFSET, *(((uint8_t *) &config) + i));
+	}
+	return i;
+}
+
---
+++ Firmware/tests/config_test.c
@@ -0,0 +1,70 @@
+/*
+ * config_test.c
+ *
+ * Created: Sa 31. Jan 2015
+ * Author: Georg von Zengen <g@vz-ge.org>
+ *
+ * Please start this test on a erased eeprom twice to verify whether the save and load function are working correctly
+ */ 
+
+#include <util/delay.h>
+#include <avr/io.h>
+
+#include "config.h"
+#include "display.h"
+#include "eeprom-config.h"
+
+
+
+
+config_t *conf;
+
+int main(void)
+{
+	display_init();
+
+	//display_digit(1,3);
+	conf = config_load();
+	uint8_t initialized = 0;
+	uint16_t i = 0;
+	for(; i < sizeof(config_t); i++){
+		if ( (*(((uint8_t*) conf) + i) != 0xff) && (*(((uint8_t*) conf) + i) != 0)){
+			initialized = 1;
+			break;
+		}
+	}
+	if(!initialized){
+		display_number(1000); //to display ERR
+		display_update();
+		conf->pid_d = 0.1;
+		conf->pid_i = 0.1;
+		conf->pid_p = 0.1;
+		conf->default_temp = 350;
+		conf->temp_offset = 50;
+		_delay_ms(1000);
+	}
+	uint16_t num = config_save();
+	display_number(num);
+	display_update();
+	_delay_ms(200);
+
+
+	while(1)
+	{
+		display_number((uint16_t)(conf->pid_p * 10));
+		display_update();	
+		_delay_us(300);		
+		display_number((uint16_t)(conf->pid_i * 10));
+		display_update();	
+		_delay_us(300);		
+		display_number((uint16_t)(conf->pid_d * 10));
+		display_update();
+		_delay_us(300);
+		display_number(conf->default_temp);
+		display_update();
+		_delay_us(300);
+		display_number(conf->temp_offset);
+		display_update();
+		_delay_us(300);
+	}
+}
