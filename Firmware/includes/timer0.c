18a34f68e5ec6885b9cc686ee0325cc99e25c677
Georg von Zengen <oni303@gmail.com>
2015-02-01 21:59:20 +0100

    Disabled fast-PWM mode
    now all digits are powered for the same amount of time and therefore
    they are equally bright

--- Firmware/includes/timer0.c
+++ Firmware/includes/timer0.c
@@ -9,17 +9,11 @@
 #include <avr/interrupt.h>
 void timer0_init(void)
 {
-	static once = 0;
+	static uint8_t once = 0;
 	if(!once){
 		once = 1;
-		TCCR0A = 0;
-		TCCR0A |= (1 << COM0A1); //Clear OC0B on Compare Match
-		TCCR0A |= (1 << WGM00) | (1 << WGM01) ; //Fast pwm
-		TCCR0B = 0;
-		TCCR0B |= (1 << WGM02); //Fast pwm
+		TCCR0A |= (1 << COM0A1); //Clear OC0A on Compare Match for pwm generation
 		TCCR0B |= (1 << CS01) | (1 << CS00); // prescaler = 64 => ~2ms to overflow
-		TIMSK0 = 0;
-		TIMSK0 |=  (1 << OCIE0B);
 		sei();
 	}
 }
