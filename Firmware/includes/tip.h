8dcba72066dfad3ac86eef474ab3b5a941a41fab
Georg von Zengen <oni303@gmail.com>
2015-02-05 10:20:33 +0100

    multiple include of pin.h is now possible
    Conflicts:
    	Firmware/includes/tip.h

--- Firmware/includes/tip.h
+++ Firmware/includes/tip.h
@@ -4,7 +4,9 @@
  * Created:  Mon Feb  2 22:31:29 2015
  * Author: Georg von Zengen <oni303@gmail.com>
  */ 
+#pragma once
 void tip_conversion_complete_callback(uint16_t adc);
 void tip_init(void(*callback)(int16_t));
 int16_t tip_get_temp(void);
+
 void tip_start_conversion(void);
