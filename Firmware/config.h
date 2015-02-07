5a297a6b80313d1840254fa635927726f6573a54
Georg von Zengen <oni303@gmail.com>
2015-02-07 16:57:07 +0100

    some config enhancements
    Added default values for PID-controller.
    Added function which save default values to the eeprom if no values are
    stored in there.

--- Firmware/config.h
+++ Firmware/config.h
@@ -3,7 +3,15 @@
 #define HW_VERSION 1
 
 // the temperature the system is supposed to not exceed
-#define TEMP_MAX 400
+#define TEMP_MAX 450
 
 // lowest temperature allowed
-#define TEMP_MIN -99
+#define TEMP_MIN 0 
+
+// default power on temperature
+#define DEFAULT_TEMP 300
+
+//PID-Controller values
+#define PID_P 128
+#define PID_I 12
+#define PID_D 12
