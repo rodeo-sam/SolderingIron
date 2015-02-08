1d5e578dcf92875c26ff52b12723d16f5777d8e8
Georg von Zengen <oni303@gmail.com>
2015-02-08 18:16:56 +0100

    Implemented a 10Hz PID controller
    This the so far most silent version. There are just a few clicks
    noticeable.

--- Firmware/includes/controller.c
+++ Firmware/includes/controller.c
@@ -7,6 +7,7 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
 #include "eeprom-config.h"
 #include "tip.h" 
@@ -17,13 +18,14 @@
 
 #include "display.h"
 
+#define Ta 100 //ms
+#define Trand ((Ta*32)/255)
 
 volatile int16_t w = 0;
 
 volatile int16_t esum = 0;
 volatile int16_t eold = 0;
 volatile int16_t y_buffer = 255;
-volatile uint8_t Trand = 255; 
 void new_temperature_ready_callback(int16_t);
 
 void control_set_temp(int16_t temp)
@@ -34,15 +36,15 @@ void control_set_temp(int16_t temp)
 void control_init(void)
 {
 	printf("controller boot\r\n");
-	timer0_init();
-	tip_init(&new_temperature_ready_callback);
+	OCR0A = Trand;
 	TCCR0A |= (1 << WGM01); //CTC mode
-	//TCCR0A |= (1 << COM0A1); //| (1 << COM0B0) | (1 << COM0B1); //clear at COMPA(MAX), set at COMPB 
-	TIMSK0 |= (1 << OCIE0A) | (1 << OCIE0B);
-	OCR0A = 255;
-	
+	TIMSK0 |= (1 << OCIE0A) ;
+	tip_init(&new_temperature_ready_callback);
+	tip_start_conversion();
 	DDRD |= (1<< PD4) ;
 	w = config.default_temp;
+	timer0_init();
+	
 }
 
 void control(int16_t temp)
@@ -56,12 +58,14 @@ void control(int16_t temp)
 	} else if(esum <= (int32_t)(-0x3fff + e)){
 		esum = (-0x3fff);
 	}else{
+		if(((y_buffer > 0) && (y_buffer < 255) ) || ((y_buffer == 255) && (e < 0)) || ( (y_buffer == 0) && (e > 0))){
 			esum += e;
+		}
 	}
 	
 	int16_t p = ((int32_t)((int32_t)config.pid_p*(int32_t)e))/(int32_t)128;
-	int16_t i = 0;//(int32_t)((int32_t)config.pid_i*(int32_t)Trand*(int32_t)esum)/(uint32_t)128;
-	int16_t d = 0;//(int32_t)(config.pid_d/Trand*(e - eold))/(uint32_t)128;
+	int16_t i = (int32_t)((int32_t)config.pid_i*(int32_t)Ta*(int32_t)esum)/(uint32_t)128;
+	int16_t d = 0;//(int32_t)(config.pid_d/Ta*(e - eold))/(uint32_t)128;
 	int16_t y = (int32_t)((int32_t)p + (int32_t)i + (int32_t)d);
 	eold = e;
 
@@ -71,51 +75,42 @@ void control(int16_t temp)
 	} else if (y < 0){
 		y = 0;
 	}
-	//printf("e: %d y: %d\r\n",e,y);
+	//printf("e: %d y: %d\r\n",esum,y);
 
 	y_buffer = y;
 }
-
+uint16_t pwm_count =0;
 uint8_t compb_state = 0;
 void new_temperature_ready_callback(int16_t temp){
-	if(compb_state == 1 && TCNT0 < OCR0B){
-		compb_state = 2;
-		TIFR0 |= (1 << OCIE0B);
-		TIMSK0 |= (1 << OCIE0B);
-	}else if(compb_state == 2 ||TCNT0 >= OCR0B ){
-		// we missed compb
-		if(y_buffer){
-			PORTD |= (1 << PD4);
-		}
-		//TCCR0A |= (1 << COM0A1);//clear pin on COMPA
-	}
-	control(temp); //do this some where else?
+	control(temp);
+	compb_state=1;
 }
 
 ISR(TIMER0_COMPA_vect) {
-	//set pwm pin low
-	PORTD &= ~(1 << PD4);
-	Trand = 16 + (get_random() % (255-16));
-	OCR0A = Trand; //set OCR0A to next pin low time
-	compb_state = 0;
-	TIFR0 |= (1 << OCIE0B);
-	TIMSK0 |= (1 << OCIE0B);
-	OCR0B = TCNT0 + 3; // wait until PWM caused offset voltate has faded from temperature sensor
-}
-
-ISR(TIMER0_COMPB_vect) {
-	if(compb_state == 0){
-		OCR0B = ((uint16_t)((uint16_t)(255-y_buffer) * (uint16_t)Trand) / 255) - 1; //set OCR0B to pin high time
+	uint8_t t = 0;
+	if((pwm_count == 0) && compb_state && (y_buffer > 2) ){
+		compb_state = 0;
+		PORTD |= (1 << PD4);
+	}else if((pwm_count == 1) && (y_buffer == 0)){
+		PORTD &= ~(1 << PD4);
+		compb_state = 0;
 		tip_start_conversion();
-		compb_state = 1;
-		TIMSK0 &= ~(1 << OCIE0B);
-	}else if(compb_state == 1){
-		OCR0B = TCNT0 + 1; //wait some more time for ADC
-	}else if(compb_state == 2){
-		//TCCR0A |= (1 << COM0A1);//clear pin on COMPA
-		//TIMSK0 &= ~(1 << OCIE0B); //disable COMPB interrupt
-		if(y_buffer){
-			PORTD |= (1 << PD4);
+	} else if((pwm_count == 0) && !compb_state ){
+		pwm_count = 0;
+		t = 1;
+	} else {
+		if(pwm_count == y_buffer){ 
+			PORTD &= ~(1 << PD4);
+		}else if((pwm_count == y_buffer + 2)){ 
+			tip_start_conversion();
 		}
 	}
+	if((compb_state == 1) && (pwm_count >= 255)){
+		pwm_count = 0;
+		t = 1;
+	}
+	if(!t){
+		pwm_count++;
+	}
 }
+
