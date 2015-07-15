931cdd906b9cad03f75017c94f1e419d7db81e77
debaal <github@van-balen.de>
2015-07-15 14:21:16 +0200

    reformatting

--- Firmware/includes/rotary_encoder.h
+++ Firmware/includes/rotary_encoder.h
@@ -3,7 +3,7 @@
 *
 * Reads rotary encoder every 2ms and calls plus/minus/button-callbacks
 *
-* Author: Keno Garlichs, <k.garlichs@tu-braunschweig.de>              
+* Author: Keno Garlichs, <k.garlichs@tu-braunschweig.de>
 */
 #pragma once
 
@@ -11,12 +11,12 @@
 #include <stdio.h>
 #include "timing.h"
 
-#define ROTARY_A     (ROTARY_A_PIN & (1 << ROTARY_A_PIN_NR))
-#define ROTARY_B     (ROTARY_B_PIN & (1 << ROTARY_B_PIN_NR))
-#define ROTARY_BUTTON_PRESSED		!(ROTARY_BUTTON_PIN & (1 << ROTARY_BUTTON_PIN_NR))
+#define ROTARY_A (ROTARY_A_PIN & (1 << ROTARY_A_PIN_NR))
+#define ROTARY_B (ROTARY_B_PIN & (1 << ROTARY_B_PIN_NR))
+#define ROTARY_BUTTON_PRESSED !(ROTARY_BUTTON_PIN & (1 << ROTARY_BUTTON_PIN_NR))
 
-#define ROTARY_A_STATE		(ROTARY_A >> (ROTARY_A_PIN_NR - 1))		// Shifts ROTARY_A bit to bit 1
-#define ROTARY_B_STATE		(ROTARY_B >> ROTARY_B_PIN_NR)		// Shifts ROTARY_B bit to bit 0
+#define ROTARY_A_STATE (ROTARY_A >> (ROTARY_A_PIN_NR - 1))		// Shifts ROTARY_A bit to bit 1
+#define ROTARY_B_STATE (ROTARY_B >> ROTARY_B_PIN_NR)		// Shifts ROTARY_B bit to bit 0
 
 extern void (*plus_callback)(void);
 extern void (*minus_callback)(void);
