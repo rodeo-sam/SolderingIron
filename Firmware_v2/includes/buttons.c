1c92299f3083310932518d4725fadf43b11f47a1
Johannes van Balen <johannes@van-balen.de>
2017-05-24 21:26:02 +0200

    moved ISR to soldering_iron.c

--- Firmware_v2/includes/buttons.c
+++ Firmware_v2/includes/buttons.c
@@ -9,9 +9,11 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdlib.h>
+#include "global_vars.h"
 #include "config.h"
 #include "buttons.h"
-#include "global_vars.h"
+#include "uart.h"
+
 
 /*
  * Initialize buttons and the corresponding interrupts
@@ -28,26 +30,6 @@ void buttons_init(void)
 	EICRA |= (1 << ISC11) | (1 << ISC01); //falling edge generates interrupt
 	EIMSK |= (1 << INT0)  | (1 << INT1); //enable interrupts
 
-	// enable global interrupts
-	sei();
-}
-
-
-
-ISR(INT0_vect) // MINUS button
-{
-	if (global_targetTemperature > MIN_TEMP) {
-		global_targetTemperature -= 10;
-		global_showTargetTemperature = 0; // reset time to show digits
-	}
-}
-
-
-
-ISR(INT1_vect) // PLUS button
-{
-	if (global_targetTemperature < MAX_TEMP) {
-		global_targetTemperature += 10;
-		global_showTargetTemperature = 0; // reset time to show digits
-	}
+  unsigned char buffer[16] = "BUTTONS - INIT\r\n";
+  uart_transmit(buffer, 16);
 }
