e2e94d8226997a523c9bc2754293c43f966b6445
Johannes van Balen <johannes@van-balen.de>
2017-05-24 21:34:57 +0200

    changed current temperature to a signed value (int16_t would probably do just fine), removed global_timer0_trigger (will be a local variable in soldering_iron.c, since ISR is also there)

--- Firmware_v2/includes/global_vars.h
+++ Firmware_v2/includes/global_vars.h
@@ -1,10 +1,22 @@
 
 #include <avr/io.h>
+#include <avr/io.h>
+#include <stdint.h>
+
 
 // globally used variables
 
 static uint16_t global_targetTemperature;
-static uint16_t global_currentTemperature;
+static int32_t global_currentTemperature;
 static uint16_t global_showTargetTemperature;
+static uint8_t global_cycle_counter;
+static uint8_t global_pwm_value;
 
-static uint8_t global_timer0_trigger;
+static uint8_t global_display_main_cycle_flag;
+static uint8_t led_idx;
+
+static uint8_t global_buttons; // 0x02: PLUS, 0x01: MINUS
+
+static uint16_t global_adc_value;
+
+static uint8_t global_tip_enabled;
