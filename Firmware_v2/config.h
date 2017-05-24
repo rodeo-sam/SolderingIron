14183a2e5cee7e4b9051929478557ba0e3797dd2
Johannes van Balen <johannes@van-balen.de>
2017-05-24 21:21:23 +0200

    introduced cycle define, pwm_start and _end value, added temperature conversion numbers, changed baudrate to 38400, add targettemperature show time in cycles

--- Firmware_v2/config.h
+++ Firmware_v2/config.h
@@ -11,15 +11,26 @@
 #define PID_I 2
 #define PID_D 10
 
+// temp measurement
+#define MEASUREMENT_EVERY_N_CYCLES 5
+
 // max temperature values
 #define MIN_TEMP 120
 #define MAX_TEMP 380
 
-// temperature is calculated from the adc reading via a linear function: temp = m * adc + b
-#define TEMP_CONVERSION_B 49.9424k
-#define TEMP_CONVERSION_M 0.4433k
+#define START_COUNT 50
+#define END_COUNT 250
+#define MAX_PWM_VALUE (END_COUNT - START_COUNT)
+
+// temperature is calculated from the adc reading via a linear function: temp = (m * adc + b)/d
+#define TEMP_CONVERSION_B 499424L
+#define TEMP_CONVERSION_M 4433L
+#define TEMP_CONVERSION_D 10000L // solely for fixpoint arithmetics
 
 // UART
-#define BAUD 38400
+#define BAUD 9600
+
+//display
+#define TARGET_TEMP_SHOW_TIME 400
 
 #include "pinout.h"
