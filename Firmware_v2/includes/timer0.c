ac13b37963b7db2b2e38e8531619d887666a3cd4
Johannes van Balen <johannes@van-balen.de>
2017-05-24 21:31:32 +0200

    added more interrupt bits to TIMSK0

--- Firmware_v2/includes/timer0.c
+++ Firmware_v2/includes/timer0.c
@@ -6,7 +6,7 @@
  */
 
 #include <avr/io.h>
-
+#include "config.h"
 #include "global_vars.h"
 #include "tip.h"
 #include "uart.h"
@@ -18,17 +18,10 @@ void timer0_init(void)
 	// timer 0
 	// used for a 8ms cycle for everything but heating control
 	TCCR0B |= (1 << CS02); // prescaler = 256 @ 8MHz -> ~122Hz -> 8ms
-}
-
-ISR(TIMER0_OVF_vect) {
-	if (!global_timer0_trigger) {
-		global_timer0_trigger = 1;
-	} else {
-		unsigned char buffer[42] = "TIMER0: LOOP OVERLAP ERROR - SHUTTING DOWN\r\n";
-	  uart_transmit(buffer, 42);
-		while(1) {
-			tip_disable();
-			// Let the watchdog bite!
-		}
-	}
+  TIMSK0 |= (1 << OCIE0A); // enable compare match interrupt A
+  TIMSK0 |= (1 << OCIE0B); // enable compare match interrupt B
+  TIMSK0 |= (1 << TOIE0); // enable overflow interrupt
+  OCR0B = END_COUNT; // Shut off tip
+  unsigned char buffer[20] = "TIMER0 - INITIATED\r\n";
+  uart_transmit(buffer, 20);
 }
